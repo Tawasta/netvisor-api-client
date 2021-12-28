e9733d931758529a84c4bea69e04dde885ea9a9b
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2021-12-28 13:29:39 +0200

    [IMP] Add "ordered" to schema

--- netvisor_api_client/schemas/sales_invoices/match_credit_note.py
+++ netvisor_api_client/schemas/sales_invoices/match_credit_note.py
@@ -6,3 +6,6 @@ from ..common import RejectUnknownFieldsSchema
 class SalesInvoiceMatchCreditNoteSchema(RejectUnknownFieldsSchema):
     credit_note_netvisor_key = fields.Integer()
     invoice_netvisor_key = fields.Integer()
+
+    class Meta:
+        ordered = True
